5c36618ecac64dbd967b4a24526f55748cb20fdf
naiara-ada <naiara.ada@gmail.com>
2023-11-20 12:49:06 +0100

    funcionando

---
+++ js/script.js
@@ -0,0 +1,60 @@
+const userList = document.getElementById('listaUsuarios');
+
+const getUsers = ()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then (response =>{
+            if (!response.ok){
+                throw new Error ('La solicitud no tuvo éxito')
+            }
+            return response.json();
+        })
+        .then (datas =>{
+            const usersData = [];
+            const newData2 = newData(); //metemos en un array los datos nuevos generados.
+            datas.forEach((data, i)=>{          //recorremos el array recogido de la apli y metemos los nuevos datos generados
+                const datos ={
+                    ...datas[i],
+                    age: newData2[i].age,
+                    image: newData2[i].image
+                }               
+                usersData.push(datos); //metemos en el array todo incluido
+            })
+            console.log(usersData);
+            for (let data of usersData){
+                const template = `
+                    <li class="contenedor"> 
+                        <div class="cabecera">                   
+                        <div class="datosPersonales">
+                            <p><span>Nombre:</span> ${data.name}</p>
+                            <p><span>Edad:</span> ${data.age}</p>
+                            <p><span>Username:</span> ${data.username}</p>
+                            <p><span>Teléfono:</span> ${data.phone}</p>
+                            <p><span>Email:</span> ${data.email}</p>
+                        </div> 
+                        <img src="${data.image}" alt="${data.name}" /></div>
+                        <p><span>Compañia:</span> ${data.company.name}</p>
+                        <p><span>Dirección:</span> ${data.address.street}, ${data.address.suite}, ${data.address.city}</p>
+                    </li>
+                `
+                userList.innerHTML += template
+
+            }
+           
+        })
+}
+
+getUsers();
+
+
+const newData =() =>{
+    const data2 =[];
+    for (let i = 1; i<=10; i++){
+        const objData ={
+            age: Math.floor(Math.random()* (66-18)+18),     // para la edad un numero aleatori entre 18 y 65
+            image: `./assets/img/${i}.jpeg`
+        }
+        data2.push(objData);    }
+   
+
+    return (data2);
+}
